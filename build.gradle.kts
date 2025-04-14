302a4192f161d5e7c657a115c319be874d214361
juhyeon1114 <juhyeon1114@gmail.com>
2025-04-14 22:12:31 +0900

    init

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+	kotlin("jvm") version "2.1.10"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+
+}
+
+tasks.test {
+	useJUnitPlatform()
+}
+kotlin {
+	jvmToolchain(21)
+}
